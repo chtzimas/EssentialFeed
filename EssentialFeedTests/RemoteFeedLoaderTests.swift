b289da8922ba76aa97099ed3b594200b01533d09
Christos Tzimas <c.tzimas@afse.eu>
2025-02-16 13:04:50 +0200

    Refactor test to remove duplication

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -9,12 +9,12 @@ import XCTest
 
 class RemoteFeedLoader {
     
-    let client: HTTPClient
     let url: URL
+    let client: HTTPClient
     
-    init(client: HTTPClient, url: URL) {
-        self.client = client
+    init(url: URL, client: HTTPClient) {
         self.url = url
+        self.client = client
     }
     
     func load() {
@@ -26,23 +26,12 @@ protocol HTTPClient {
     func get(from url: URL)
 }
 
-class HTTPClientSpy: HTTPClient {
-    
-    var requestedURL: URL?
-    
-    func get(from url: URL) {
-        requestedURL = url
-    }
-}
-
 class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init_doesNotRequestDataFromURL() {
         
         // Given
-        let client = HTTPClientSpy()
-        let url = URL(string: "https://a-url.com")!
-        _ = RemoteFeedLoader(client: client, url: url)
+        let (_, client) = makeSUT()
         
         // Then
         XCTAssertNil(client.requestedURL)
@@ -51,14 +40,30 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_requestDataFromURL() {
         
         // Given
-        let client = HTTPClientSpy()
-        let url = URL(string: "https://a-url.com")!
-        let sut = RemoteFeedLoader(client: client, url: url)
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
         
         // When
         sut.load()
         
         // Then
         XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
     }
 }

0eec6c6015952b3a50c417c2513d4df8f56d216f
Christos Tzimas <c.tzimas@afse.eu>
2025-02-20 12:45:41 +0200

    Move Decodable logic to a new private Item struct to decouple the Feed Feature module from API implementation details

--- EssentialFeed/Feed Api/RemoteFeedLoader.swift
+++ EssentialFeed/Feed Api/RemoteFeedLoader.swift
@@ -31,10 +31,6 @@ public final class RemoteFeedLoader {
         case failure(Error)
     }
     
-    private struct Root: Decodable {
-        let items: [FeedItem]
-    }
-    
     public init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
@@ -45,7 +41,8 @@ public final class RemoteFeedLoader {
             switch result {
             case let .success(data, response):
                 if response.statusCode == 200, let root = try? JSONDecoder().decode(Root.self, from: data) {
-                    completion(.success(root.items))
+                    let items = root.items.map { $0.item }
+                    completion(.success(items))
                 } else {
                     completion(.failure(.invalidData))
                 }
@@ -53,5 +50,24 @@ public final class RemoteFeedLoader {
                 completion(.failure(.connectivity))
             }
         }
+    }
+}
+
+private struct Root: Decodable {
+    let items: [Item]
+}
+
+private struct Item: Decodable {
+    
+    let id: UUID
+    let description: String?
+    let location: String?
+    let image: URL
+    
+    var item: FeedItem {
+        FeedItem(id: id,
+                 description: description,
+                 location: location,
+                 imageURL: image)
     }
 }

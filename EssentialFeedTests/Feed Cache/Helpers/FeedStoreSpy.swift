3def929778a4f6d99bb09eb1fdd10e03c13e5680
Christos Tzimas <c.tzimas@afse.eu>
2025-03-20 18:46:50 +0200

    Load command requests cache retrieval

--- EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -13,6 +13,7 @@ class FeedStoreSpy: FeedStore {
     enum ReceivedMessage: Equatable {
         case deleteCachedFeed
         case insert([LocalFeedImage], Date)
+        case retrieve
     }
     
     private(set) var receivedMessages = [ReceivedMessage]()
@@ -28,6 +29,10 @@ class FeedStoreSpy: FeedStore {
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         receivedMessages.append(.insert(feed, timestamp))
         insertionCompletions.append(completion)
+    }
+    
+    func retrieve() {
+        receivedMessages.append(.retrieve)
     }
     
     func completeDeletion(with error: Error, at index: Int = 0) {

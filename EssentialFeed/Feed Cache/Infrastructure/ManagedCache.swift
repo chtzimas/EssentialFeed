5fe0021ad41d194c2a0e24c11700a3242830de57
Christos Tzimas <tzimasc@piraeusbank.gr>
2025-04-24 08:41:19 +0300

    Separate CoreData managed model classes data from helpers with extensions

--- EssentialFeed/Feed Cache/Infrastructure/ManagedCache.swift
+++ EssentialFeed/Feed Cache/Infrastructure/ManagedCache.swift
@@ -9,9 +9,11 @@ import CoreData
 
 @objc(ManagedCache)
 internal class ManagedCache: NSManagedObject {
-    
     @NSManaged internal var timestamp: Date
     @NSManaged internal var feed: NSOrderedSet
+}
+
+extension ManagedCache {
     
     internal static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
         let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
--- EssentialFeed/Feed Cache/Infrastructure/ManagedFeedImage.swift
+++ EssentialFeed/Feed Cache/Infrastructure/ManagedFeedImage.swift
@@ -9,12 +9,14 @@ import CoreData
 
 @objc(ManagedFeedImage)
 internal class ManagedFeedImage: NSManagedObject {
-    
     @NSManaged internal var id: UUID
     @NSManaged internal var imageDescription: String?
     @NSManaged internal var location: String?
     @NSManaged internal var url: URL
     @NSManaged internal var cache: ManagedCache
+}
+
+extension ManagedFeedImage {
     
     internal static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
         NSOrderedSet(array: localFeed.map { local in

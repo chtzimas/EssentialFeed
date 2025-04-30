7e0c3988c0e5413c7c2a10a24778f862df511423
Christos Tzimas <tzimasc@piraeusbank.gr>
2025-04-30 16:34:58 +0300

    Extract duplicate cache load expectations into a shared helper method

--- EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
+++ EssentialFeedCacheIntegrationTests/EssentialFeedCacheIntegrationTests.swift
@@ -23,21 +23,8 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
     func test_load_deliversNoItemsOnEmptyCache() {
         
         let sut = makeSUT()
-        let exp = expectation(description: "Wait for load completion")
         
-        sut.load { result in
-            switch result {
-            case let .success(imageFeed):
-                XCTAssertEqual(imageFeed, [], "Expected empty feed")
-
-            case let .failure(error):
-                XCTFail("Expected successful feed result, got \(error) instead")
-            }
-
-            exp.fulfill()
-        }
-        
-        wait(for: [exp], timeout: 1.0)
+        expect(sut, toLoad: [])
     }
     
     func test_load_deliversItemsSavedOnASeparateInstance() {
@@ -54,20 +41,7 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
         
         wait(for: [saveExp], timeout: 1.0)
 
-        let loadExp = expectation(description: "Wait for load completion")
-        sutToPerformLoad.load { loadResult in
-            switch loadResult {
-            case let .success(imageFeed):
-                XCTAssertEqual(imageFeed, feed, "Expected `sutToPerformLoad` instance to retrieve the feed saved by `sutToPerformSave` instance")
-
-            case let .failure(error):
-                XCTFail("Expected successful feed result, got \(error) instead")
-            }
-
-            loadExp.fulfill()
-        }
-        
-        wait(for: [loadExp], timeout: 1.0)
+        expect(sutToPerformLoad, toLoad: feed)
     }
     
     // MARK: Helpers
@@ -80,6 +54,25 @@ final class EssentialFeedCacheIntegrationTests: XCTestCase {
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func expect(_ sut: LocalFeedLoader, toLoad expectedFeed: [FeedImage], file: StaticString = #file, line: UInt = #line) {
+        
+        let exp = expectation(description: "Wait for load completion")
+        
+        sut.load { loadResult in
+            switch loadResult {
+            case let .success(imageFeed):
+                XCTAssertEqual(imageFeed, expectedFeed, "Expected `sutToPerformLoad` instance to retrieve the feed saved by `sutToPerformSave` instance")
+                
+            case let .failure(error):
+                XCTFail("Expected successful feed result, got \(error) instead")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
     }
     
     private func setupEmptyStoreState() {

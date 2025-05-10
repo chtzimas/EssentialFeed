5f4dca53e0a9ff69c434d14484f6912446686b65
Christos Tzimas <tzimasc@piraeusbank.gr>
2025-05-10 17:05:07 +0300

    Add typealias for `FeedStore.DeletionResult` and `FeedStore.InsertionResult`

--- EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -11,8 +11,11 @@ public typealias CacheFeed = (feed: [LocalFeedImage], timestamp: Date)
 
 public protocol FeedStore {
     
-    typealias DeletionCompletion = (Error?) -> Void
-    typealias InsertionCompletion = (Error?) -> Void
+    typealias DeletionResult = Error?
+    typealias DeletionCompletion = (DeletionResult) -> Void
+    
+    typealias InsertionResult = Error?
+    typealias InsertionCompletion = (InsertionResult) -> Void
     
     typealias RetrievalResult = Result<CacheFeed?, Error>
     typealias RetrievalCompletion = (RetrievalResult) -> Void

a96bbb7b3049b9c62c18c7088f46efcab75c3fce
Christos Tzimas <tzimasc@piraeusbank.gr>
2025-05-28 08:05:56 +0300

    Extract `UIImage` test helper to a separate file

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -504,18 +504,6 @@ private extension UIRefreshControl {
     }
 }
 
-private extension UIImage {
-    static func make(withColor color: UIColor) -> UIImage {
-        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-        let format = UIGraphicsImageRendererFormat()
-        format.scale = 1
-        return UIGraphicsImageRenderer(size: rect.size, format: format).image { rendererContext in
-            color.setFill()
-            rendererContext.fill(rect)
-        }
-    }
-}
-
 private class FakeRefreshControl: UIRefreshControl {
     
     private var _isRefreshing = false

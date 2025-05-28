94a21de602d69732eed37c445fe3bd2007d6581b
Christos Tzimas <tzimasc@piraeusbank.gr>
2025-05-28 08:27:53 +0300

    Mark `LoaderSpy` as `final`

--- EssentialFeediOSTests/Helpers/FeedViewControllerTests+LoaderSpy.swift
+++ EssentialFeediOSTests/Helpers/FeedViewControllerTests+LoaderSpy.swift
@@ -11,7 +11,7 @@ import Foundation
 
 extension FeedViewControllerTests {
     
-    final class LoaderSpy: FeedLoader, FeedImageDataLoader {
+    class LoaderSpy: FeedLoader, FeedImageDataLoader {
         
         // MARK: - FeedLoader
         

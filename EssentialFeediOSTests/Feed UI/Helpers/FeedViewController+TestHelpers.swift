880c988c2c78366ae7ca7529b93cbbe59ebab992
Christos Tzimas <tzimasc@piraeusbank.gr>
2025-07-06 17:00:39 +0300

    Move error view configuration to storyboard

--- EssentialFeediOSTests/Feed UI/Helpers/FeedViewController+TestHelpers.swift
+++ EssentialFeediOSTests/Feed UI/Helpers/FeedViewController+TestHelpers.swift
@@ -49,7 +49,7 @@ extension FeedViewController {
     }
     
     var errorMessage: String? {
-        errorView.message
+        errorView?.message
     }
     
     var isShowingLoadingIndicator: Bool {

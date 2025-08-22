9a85dafdd1ca68c811e3931fe844682ef4137414
Christos Tzimas <tzimasc@piraeusbank.gr>
2025-08-22 19:24:56 +0300

    Extract test helpers into a shared scope to remove duplication

--- EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderWithFallbackCompositeTests.swift
@@ -134,19 +134,7 @@ final class FeedImageDataLoaderWithFallbackCompositeTests: XCTestCase {
         
         wait(for: [exp], timeout: 1.0)
     }
-
-    func anyData() -> Data {
-        Data("any data".utf8)
-    }
     
-    private func anyURL() -> URL {
-        URL(string: "http://a-url.com")!
-    }
-    
-    private func anyNSError() -> NSError {
-        NSError(domain: "any error", code: 0)
-    }
-
     private class LoaderSpy: FeedImageDataLoader {
         
         private var messages = [(url: URL, completion: (FeedImageDataLoader.Result) -> Void)]()

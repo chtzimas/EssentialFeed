4e41f28f0cff2db04adafe30af0d2c0072b3f23f
Christos Tzimas <tzimasc@piraeusbank.gr>
2025-08-22 18:45:38 +0300

    `FeedLoaderWithFallbackComposite.load` delivers fallback feed on primary loader failure

--- EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -11,13 +11,23 @@ import EssentialFeed
 class FeedLoaderWithFallbackComposite: FeedLoader {
     
     private let primary: FeedLoader
+    private let fallback: FeedLoader
 
     init(primary: FeedLoader, fallback: FeedLoader) {
         self.primary = primary
+        self.fallback = fallback
     }
 
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        primary.load(completion: completion)
+        primary.load { [weak self] result in
+            switch result {
+            case .success:
+                completion(result)
+                
+            case .failure:
+                self?.fallback.load(completion: completion)
+            }
+        }
     }
 }
 
@@ -43,6 +53,28 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
 		}
 		wait(for: [exp], timeout: 1)
 	}
+    
+    func test_load_deliversFallbackFeedOnPrimaryLoaderFailure() {
+        
+        let fallbackFeed = uniqueFeed()
+        let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .success(fallbackFeed))
+
+        let exp = expectation(description: "Wait for load completion")
+
+        sut.load { result in
+            switch result {
+            case let .success(receivedFeed):
+                XCTAssertEqual(receivedFeed, fallbackFeed)
+
+            case .failure:
+                XCTFail("Expected successful load feed result, got \(result) instead")
+            }
+
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 1.0)
+    }
 	
 	// MARK: - Helpers
 	
@@ -61,9 +93,13 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
 			XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
 		}
 	}
+    
+    private func anyNSError() -> NSError {
+        NSError(domain: "any error", code: 0)
+    }
 	
 	private func uniqueFeed() -> [FeedImage] {
-		return [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "http://any-url.com")!)]
+		[FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "http://any-url.com")!)]
 	}
 
     private class LoaderStub: FeedLoader {

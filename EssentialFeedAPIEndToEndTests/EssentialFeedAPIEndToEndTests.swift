5be3991de251efa838381f81af2210332b3f306a
Christos Tzimas <tzimas13@hotmail.com>
2025-11-10 15:45:28 +0200

    Move RemoteLoader composition to the Composition Root

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -47,15 +47,14 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
     
     // MARK: - Helpers
     
-    private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> RemoteFeedLoader.Result? {
+    private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> FeedLoader.Result? {
         
-        let client = ephemeralClient(file: file, line: line)
-        let loader = RemoteFeedLoader(url: feedTestServerURL, client: client)
+        let loader = RemoteLoader(url: feedTestServerURL, client: ephemeralClient(), mapper: FeedItemsMapper.map)
         trackForMemoryLeaks(loader, file: file, line: line)
         
         let exp = expectation(description: "Wait for load completion")
         
-        var receivedResult: RemoteFeedLoader.Result?
+        var receivedResult: FeedLoader.Result?
         
         loader.load { result in
             receivedResult = result
